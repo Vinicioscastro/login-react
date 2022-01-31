05929d82fcde20b5a4d1e26934d4c28058a27357
vinicioscastro <viniciospcastro19@gmail.com>
2022-01-31 18:08:40 -0300

    flexbox

--- src/Login.js
+++ src/Login.js
@@ -1,23 +1,24 @@
-import React from "react"
-import './Login.css'
+import React from "react";
+import "./Login.css";
 
-export default function Login (){
-    return(
-        <div className="tela">
-            <div className="login">
-                <h1>Login</h1>
-                <input type="email" placeholder="Email" autoFocus/>
-                <br/>  <br/> 
-                <input type="password" placeholder="Senha"/>
-                <br/>  <br/> 
-                <input type="checkbox" className="boxx" /> <label>Lembra Senha</label>
-                <br/> <br/>
-                <button>Entrar</button>
-                <br/> <br/>
-                <a href=""> Cadastro de novo usuario</a>
-            </div>
+export default function Login() {
+  return (
+    <div className="tela">
+      <div className="login">
+        <h1>Login</h1>
+        <input type="email" placeholder="Email" autoFocus />
+        <br /> <br />
+        <input type="password" placeholder="Senha" />
+        <br />
+        <div className="senha">
+          <input type="checkbox" className="boxx" />
+          <label>Lembra Senha</label>
         </div>
-        
-    )
+        <br /> <br />
+        <button>Entrar</button>
+        <br /> <br />
+        <a href=""> Cadastro de novo usuario</a>
+      </div>
+    </div>
+  );
 }
-
